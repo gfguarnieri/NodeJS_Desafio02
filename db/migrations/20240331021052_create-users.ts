9a902b9ef4ab734e6b52e36d61424f38bf7cf8a2
Giovanni Guarnieri <gfguarnieri@hotmail.com>
2024-03-30 23:27:59 -0300

    :wrench: chore: criação das Migrations para criação das tabelas

---
+++ db/migrations/20240331021052_create-users.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.uuid('session_id').index()
+    table.text('name').notNullable()
+    table.text('email')
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
---
+++ db/migrations/20240331021650_create-meals.ts
@@ -0,0 +1,17 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description')
+    table.boolean('in_diet').notNullable()
+    table.uuid('userid').notNullable()
+    table.foreign('userid').references('id').inTable('users')
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
