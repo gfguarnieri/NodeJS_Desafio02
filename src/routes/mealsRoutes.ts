bd7e41a3b3d1e411f712ff5688b823134f5cb2e2
Giovanni Guarnieri <gfguarnieri@hotmail.com>
2024-03-31 12:18:07 -0300

    :bug: fix: correção na quantidade de refeições fora da dieta

--- src/routes/mealsRoutes.ts
+++ src/routes/mealsRoutes.ts
@@ -163,8 +163,18 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(401).send({ error: 'Unauthorized' })
     }
 
-    const allMeals = await knex('meals')
+    const countOffDiet = await knex('meals')
+      .where({ userid: user.id, in_diet: false })
+      .count('id', { as: 'count' })
+      .first()
+
+    const countOnDiet = await knex('meals')
       .where({ userid: user.id, in_diet: true })
+      .count('id', { as: 'count' })
+      .first()
+
+    const allMeals = await knex('meals')
+      .where({ userid: user.id })
       .orderBy('created_at', 'asc')
       .select()
 
@@ -182,11 +192,6 @@ export async function mealsRoutes(app: FastifyInstance) {
       }
     })
 
-    const countOffDiet = await knex('meals')
-      .where({ userid: user.id, in_diet: false })
-      .count('id', { as: 'count' })
-      .first()
-
     const countAll = await knex('meals')
       .where({ userid: user.id })
       .count('id', { as: 'count' })
@@ -194,7 +199,7 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     return reply.send({
       totalMeals: countAll?.count,
-      onDiet: allMeals?.length,
+      onDiet: countOnDiet?.count,
       offDiet: countOffDiet?.count,
       bestSequence: maxSequence,
     })
